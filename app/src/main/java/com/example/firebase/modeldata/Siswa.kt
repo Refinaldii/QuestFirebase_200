3f190b36ceedfd462c51819ec77bad815cca56fe
Refinaldii <refinaldi9786@gmail.com>
2026-01-03 00:45:48 +0700

    Membuat dun siswa ti uistatesiswa

--- app/src/main/java/com/example/firebase/modeldata/Siswa.kt
+++ app/src/main/java/com/example/firebase/modeldata/Siswa.kt
@@ -21,3 +21,6 @@ data class UIStateSiswa(
     val detailSiswa: DetailSiswa = DetailSiswa(),
     val isEntryValid : Boolean = false
 )
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false) : UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid)
